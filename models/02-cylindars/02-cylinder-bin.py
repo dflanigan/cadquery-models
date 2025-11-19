2d435cff1f5c3f4896d1001dbdd3291e03465852
dennisflanigan <flanigan@fluth.net>
2025-11-18 21:30:59 -0700

    Cylinder Bin

--- models/02-cylindars/02-cylinder-bin.py
+++ models/02-cylindars/02-cylinder-bin.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     print("Creating bin")
 
-    result = make_cylinder_bin(height=60.0, radius=15.0, wall=3.0)
+    result = make_cylinder_bin(height=60.0, radius=25.0, wall=3.0)
 
     print("Showing result")
 
